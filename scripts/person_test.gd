8b10a9aef5fbe19b33f8933481b527a3d5b39e76
TyrannoJ <julius.megerle@outlook.de>
2026-07-28 12:27:05 +0200

    update to godot 4.7

--- scripts/person_test.gd
+++ scripts/person_test.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
 func _process(delta: float) -> void:
-	$AnimationPlayer.play("rig|Walk")
+	$AnimationPlayer.speed_scale=2
+	$AnimationPlayer.play("Walk")
+	
 	#$rig/Skeleton3D/SkeletonIK3D.start()
